7c47ae6f8df0e98607ed8b265c5a099e0c4f9991
Mokhtar Aouf <mokhtar.aoufyt@gmail.com>
2026-03-26 16:23:28 +0000

    fini intermédiaire

---
+++ definitions/sources/declaration.js
@@ -0,0 +1,17 @@
+declare({
+  database: "dataform-usecase-491410",
+  schema: "raw_data",
+  name: "source_a_ventes",
+});
+
+declare({
+  database: "dataform-usecase-491410",
+  schema: "raw_data",
+  name: "source_a_discounts",
+});
+
+declare({
+  database: "dataform-usecase-491410",
+  schema: "raw_data",
+  name: "source_b_ecom",
+});
